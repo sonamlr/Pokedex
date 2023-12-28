95315231d0efd63c8c02c32f45070858ca95964a
Sonam <sonamlodhi6251@gmail.com>
2023-12-28 16:37:55 +0530

    pokedex

---
+++ src/components/Pokedex/Pokedex.jsx
@@ -0,0 +1,11 @@
+import Search from "../Search/Search";
+import './Pokedex.css'
+function Pokedex (){
+    return(
+        <div className="pokedex-wrapper">
+            <h1 className="pokedex-heading">Pokedex</h1>
+            <Search />
+        </div>
+    )
+}
+export default Pokedex;
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,9 @@
+import './Search.css'
+function Search() {
+    return (
+        <div className="search-wrapper">
+            <input type="text" id='pokedex-name-search' placeholder="pokedex name..." />
+        </div>
+    )
+}
+export default Search;
